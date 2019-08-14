b26e4f372e1cffe6ca40824bba244254003acf32
Borisz <vorosborisz@gmail.com>
2019-08-14 13:46:11 +0200

    faq template added

---
+++ sites/all/themes/tesla/templates/page--faq.tpl.php
@@ -0,0 +1,13 @@
+<?php $node = node_load(11); ?>
+<div id="first-row-wrapper">
+    <div class="services-title"><?php print $node->title; ?></div>
+    <div class="services-description">
+    <?php print $node->field_lead['und'][0]['value']; ?>
+    </div>
+</div>
+<div id="second-row-wrapper">
+    <div id="second-row">
+        <?php print $node->field_body['und'][0]['value']; ?>
+    </div>
+</div>
+    
